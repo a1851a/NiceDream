8be1fd97152a3c8b1509e034ea7f6295a20bd23d
lisa <a1851a111113@gmail.com>
2022-10-01 14:12:37 +0800

    fb帳號登入(目前fb開發有問題，故無法使用)

--- app/src/main/java/fju/project/nicedream/ui/login/LoginActivity.java
+++ app/src/main/java/fju/project/nicedream/ui/login/LoginActivity.java
@@ -1,7 +1,11 @@
 package fju.project.nicedream.ui.login;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
@@ -11,6 +15,13 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -20,10 +31,13 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 import java.util.Objects;
 
@@ -34,10 +48,12 @@ import fju.project.nicedream.MainActivity;
 import fju.project.nicedream.R;
 import fju.project.nicedream.data.util.DeviceChecker;
 
+
 public class LoginActivity extends AppCompatActivity {
 
     public static final String TAG = "LoginActivity";
     private GoogleSignInClient mGoogleSignInClient;
+    private CallbackManager mCallbackManager;
     private FirebaseAuth mAuth;
 
     @BindView(R.id.login_logo)
@@ -97,7 +113,28 @@ public class LoginActivity extends AppCompatActivity {
                     return;
                 }
                 else {
+                    //初始化fb sdk
+                    FacebookSdk.sdkInitialize(LoginActivity.this);
+                    // Initialize Facebook Login button
+                    mCallbackManager = CallbackManager.Factory.create();
+                    LoginManager.getInstance().logInWithReadPermissions(LoginActivity.this, Arrays.asList("email","public_profile"));
+                    LoginManager.getInstance().registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+                        @Override
+                        public void onSuccess(LoginResult loginResult) {
+                            Log.d(TAG, "facebook:onSuccess:" + loginResult);
+                            handleFacebookAccessToken(loginResult.getAccessToken());
+                        }
 
+                        @Override
+                        public void onCancel() {
+                            Log.d(TAG, "facebook:onCancel");
+                        }
+
+                        @Override
+                        public void onError(FacebookException error) {
+                            Log.d(TAG, "facebook:onError", error);
+                        }
+                    });
                 }
                 break;
             case R.id.login_line:
@@ -105,7 +142,8 @@ public class LoginActivity extends AppCompatActivity {
                     return;
                 }
                 else {
-
+                    startActivity(new Intent(LoginActivity.this,MainActivity.class));
+                    LoginActivity.this.finish();
                 }
                 break;
         }
@@ -124,9 +162,33 @@ public class LoginActivity extends AppCompatActivity {
         }
     }
 
+    private void handleFacebookAccessToken(AccessToken token) {
+        Log.d(TAG, "handleFacebookAccessToken:" + token);
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                            startActivity(new Intent(LoginActivity.this,MainActivity.class));
+                            LoginActivity.this.finish();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(LoginActivity.this,"登入失敗",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
     private void FirebaseGoogleAuth(GoogleSignInAccount account) {
         AuthCredential authCredential = GoogleAuthProvider.getCredential(account.getIdToken(),null);
-        mAuth.signInWithCredential(authCredential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+        mAuth.signInWithCredential(authCredential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if (task.isSuccessful()){
@@ -157,8 +219,10 @@ public class LoginActivity extends AppCompatActivity {
             super.onActivityResult(requestCode, resultCode, data);
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
             handleGoogleAccessToken(task);
-            startActivity(new Intent(LoginActivity.this,MainActivity.class));
-            LoginActivity.this.finish();
+        }
+        else {
+            super.onActivityResult(requestCode, resultCode, data);
+            mCallbackManager.onActivityResult(requestCode, resultCode, data);
         }
     }
 
